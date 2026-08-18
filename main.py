883d8e650f9be0ac6cd2f0a54bbbdad2b4cd62d3
Transfer Kharkiv <kharkivtransfer@gmail.com>
2026-08-18 12:28:28 +0300

    fix: use PORT 8080 as default

--- main.py
+++ main.py
@@ -124,5 +124,5 @@ async def cmd_test(message: types.Message):
     )
 
 if __name__ == "__main__":
-    port = int(os.environ.get("PORT", 8000))
+    port = int(os.environ.get("PORT", 8080))
     uvicorn.run("main:app", host="0.0.0.0", port=port)
